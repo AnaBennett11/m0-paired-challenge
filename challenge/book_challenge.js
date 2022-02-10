7b7f49b4530bfcd610fbf1631adc134372d38438
Ana Bennett <anabennett1111@gmail.com>
2022-02-10 11:09:30 -0500

    add book challenge

---
+++ challenge/book_challenge.js
@@ -0,0 +1,13 @@
+//This is a paired challenge about books and libraries.
+var numberBooks = 5;
+var favBookTitle = "To Kill a Mockingbird";
+var bookFiction = true;
+//Book is fictional.
+var numberLibraries = 6;
+var leastFavBookTitle = "The Great Gatsby";
+var openLibrary = false;
+//The library is currently closed.
+
+var libaryLocation = ["Denver", "Seattle", "San Francisco"];
+//Loactions of libraries in bigger cities.
+//Declaring variable names related to books and libraires.
---
+++ challenge/index.md
@@ -0,0 +1,6 @@
+# A list of 5 books
+1. To Kill a Mockingbird
+2. The Great Gatsby
+3. Don Quixote
+4. A Passage to India
+5. Beloved
